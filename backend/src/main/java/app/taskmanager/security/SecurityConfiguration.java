2873171a59231925b4fcbd801446be3cad996e2d
VEugen-Andrei <v.eugenandrei@gmail.com>
2023-09-27 21:28:11 +0300

    fixed API methods to retrieve token from LocalStorageService

--- backend/src/main/java/app/taskmanager/security/SecurityConfiguration.java
+++ backend/src/main/java/app/taskmanager/security/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -21,33 +22,18 @@ public class SecurityConfiguration implements WebMvcConfigurer {
   private final JwtAuthenticationFilter jwtAuthenticationFilter;
   private final AuthenticationProvider authenticationProvider;
 
-
+//  "/api/project/**", "/api/task/**"
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
       .cors(withDefaults())
-//
-//                .addFilterBefore(new CorsFilter(), ChannelProcessingFilter.class)
-      .csrf(csrf -> csrf.disable())
-      .authorizeHttpRequests(auth -> auth.requestMatchers("/api/v1/auth/**", "/api/users/**", "/api/**", "/project-container/**").permitAll().anyRequest().authenticated())
+      .csrf(AbstractHttpConfigurer::disable)
+      .authorizeHttpRequests(auth -> auth.requestMatchers("/api/v1/auth/**").permitAll().anyRequest().authenticated())
       .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
       .authenticationProvider(authenticationProvider)
       .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
     return http.build();
   }
-
-//    @Bean
-//    CorsConfigurationSource corsConfigurationSource() {
-//        CorsConfiguration configuration = new CorsConfiguration();
-////        configuration.setAllowCredentials(true);
-////        configuration.setAllowedOriginPatterns(Collections.singletonList("*"));
-//        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200/"));
-//        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "OPTIONS", "DELETE"));
-////        configuration.setAllowedHeaders(List.of("Authorization"));
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", configuration);
-//        return source;
-//    }
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,48 +1,91 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { LocalStorageService } from './localstorage.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
   private apiUrl = 'https://localhost:443/api';
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private localStorage: LocalStorageService
+  ) {}
 
-  createProject(data: any): Observable<any> {
-    return this.http.post(this.apiUrl + '/project', data);
+  private getHeaders(token?: string) {
+    token = this.localStorage.getUser()?.token;
+    if (token) {
+      const headers = new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      });
+      return headers;
+    }
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+    return headers;
   }
 
-  deleteProjectById(id: number): Observable<any> {
-    return this.http.delete(this.apiUrl + '/project/' + id);
+  createProject(data: any, token?: string | null): Observable<any> {
+    const headers = token ? this.getHeaders(token) : this.getHeaders();
+    return this.http.post(this.apiUrl + '/project', data, {
+      headers: headers,
+    });
   }
 
-  updateProjectTitle(id: number, title: string): Observable<any> {
+  deleteProjectById(id: number, token?: string | null): Observable<any> {
+    const headers = token ? this.getHeaders(token) : this.getHeaders();
+    return this.http.delete(this.apiUrl + '/project/' + id, {
+      headers: headers,
+    });
+  }
+
+  updateProjectTitle(
+    id: number,
+    title: string,
+    token?: string | null
+  ): Observable<any> {
     const body = { title: title };
-    return this.http.patch(this.apiUrl + '/project/' + id, body);
+    const headers = token ? this.getHeaders(token) : this.getHeaders();
+    return this.http.patch(this.apiUrl + '/project/' + id, body, {
+      headers: headers,
+    });
   }
 
-  getAllProjects(): Observable<any> {
-    return this.http.get(this.apiUrl + '/project/all');
+  getProjectsByUserId(userId: number, token?: string | null): Observable<any> {
+    const headers = token ? this.getHeaders(token) : this.getHeaders();
+    return this.http.get(this.apiUrl + '/project?userId=' + userId, {
+      headers: headers,
+    });
   }
 
-  getProjectsByUserId(userId: number): Observable<any> {
-    return this.http.get(this.apiUrl + '/project?userId=' + userId);
+  createTask(data: any, token?: string | null): Observable<any> {
+    const headers = token ? this.getHeaders(token) : this.getHeaders();
+    return this.http.post(this.apiUrl + '/task', data, {
+      headers: headers,
+    });
   }
 
-  createTask(data: any): Observable<any> {
-    return this.http.post(this.apiUrl + '/task', data);
+  deleteTaskById(id: number, token?: string | null): Observable<any> {
+    const headers = token ? this.getHeaders(token) : this.getHeaders();
+    return this.http.delete(this.apiUrl + '/task/' + id, {
+      headers: headers,
+    });
   }
 
-  deleteTaskById(id: number): Observable<any> {
-    return this.http.delete(this.apiUrl + '/task/' + id);
+  showTasksByProjectId(projectId: number, token?: string | null) {
+    const headers = token ? this.getHeaders(token) : this.getHeaders();
+    return this.http.get(this.apiUrl + '/task?projectId=' + projectId, {
+      headers: headers,
+    });
   }
 
-  showTasksByProjectId(projectId: number) {
-    return this.http.get(this.apiUrl + '/task?projectId=' + projectId);
-  }
-
-  updateTask(id: number, data: any) {
-    return this.http.put(this.apiUrl + '/task/' + id, data);
+  updateTask(id: number, data: any, token?: string | null) {
+    const headers = token ? this.getHeaders(token) : this.getHeaders();
+    return this.http.put(this.apiUrl + '/task/' + id, data, {
+      headers: headers,
+    });
   }
 }
